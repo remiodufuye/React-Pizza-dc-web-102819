547da57e63edfac4b2086bd60fe1f2ba1d4ecf11
github username <github email address>
2020-07-15 06:27:18 -0400

    Done.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,13 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 
 class Header extends Component {
-
   render() {
     return (
       <Fragment>
-        <h1 className="text-center">Welcome to Flatiron School's Pizzeria</h1>
+        <h1 className="text-center">Welcome to My Pizzeria</h1>
       </Fragment>
     );
   }
-
 }
 
 export default Header;
